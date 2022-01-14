7ca08ee51d0b1ab51eecf5c157f58d65dd5e118d
Duc Trung <ductrung0429.acc@gmail.com>
2022-01-14 23:44:19 +0700

    Fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   publicRuntimeConfig: {
     API_DEVELOPMENT: 'http://localhost:5000',
     API_PRODUCTION: 'https://dtchatapp.herokuapp.com',
-    PRODUCTION: true
+    PRODUCTION: false
   },
   env: {
     PUBLIC_FOLDER: 'https://dtchatapp.herokuapp.com/images/'
